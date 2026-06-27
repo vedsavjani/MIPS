e4aedf1b4beefc2a6506fc3d6f02d15683b85cff
vedsavjani <veddsavjani@gmail.com>
2026-06-27 15:43:08 +0530

    added alu module

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,10 +1,26 @@
 module alu(
     input [31:0] srca, srcb,
     input [2:0] alucontrol,
-    output [31:0] aluresult,
+    output reg [31:0] aluresult,
     output zero
     );
 
-    
+    always @(*) begin
+        case(alucontrol) 
+            3'b000 : aluresult = srca & srcb;
+            3'b001 : aluresult = srca | srcb;
+            3'b010 : aluresult = srca + srcb;
+            3'b011 : aluresult = 32'bx; // not used, so dont care
+            3'b100 : aluresult = srca & ~srcb;
+            3'b101 : aluresult = srca | ~srcb;
+            3'b110 : aluresult = srca - srcb;
+            // 3'b111 : aluresult = (srca-srcb <0)? 32'b1 : 32'b0; this wont work becoz 
+            // srca and srcb are declared unsigned (by default). so you have to extract
+            // the sign bit from srca-srcb, if its 1, then its negative, else positive
+            3'b111 : aluresult = (srca - srcb) >> 31;
+        endcase
+    end 
+
+    assign zero = (aluresult==0);
 
 endmodule
