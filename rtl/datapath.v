8b1ae5da5d30604b43cc0766665c8e82750a30de
vedsavjani <veddsavjani@gmail.com>
2026-06-30 12:00:22 +0530

    added 4:1 mux for srcb

--- rtl/datapath.v
+++ rtl/datapath.v
@@ -1,23 +1,36 @@
 module datapath(
     input clk, reset,
-    input regwrite, alusrc,
-    input regdst, memtoreg,
-    input pcsrc, jump,
-    input [2:0] alucontrol,
-    input [31:0] instr,
     input [31:0] readdata,
+    input memtoreg, regdst,
+    input IorD,
+    input [1:0] pcsrc,
+    input [1:0] alusrcb,
+    input alusrca,
+    input IRwrite,
+    input regwrite,
+    input PCEn,
     output zero,
-    output [31:0] pc,
-    output [31:0] writedata, aluout
+    output [2:0] alucontrol,
+    output [31:0] adr
     );
 
     wire  [4:0] writereg;
-    wire [31:0] pcnext, pcplus4, pcnextbr, pcbranch;
+    wire [31:0] pc, pcnext;
     wire [31:0] signimm, signimmsh;
     wire [31:0] srca, srcb;
     wire [31:0] result;
 
     // Next PC logic
-    
+    flopenr pcreg(.clk(clk), .reset(reset), .ena(PCEn), .d(pcnext), .q(pc));
+
+
+
+    // Register file logic
+
+
+
+
+    // ALU logic
+    mux2 #
 
 endmodule
