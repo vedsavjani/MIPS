0e93d7efed02e12a8707b9d91b6df27c71bb1275
vedsavjani <veddsavjani@gmail.com>
2026-07-02 16:39:53 +0530

    modified controller for pipelined proc

--- rtl/controller.v
+++ rtl/controller.v
@@ -1,4 +1,5 @@
 module controller(
+    input clk,
     input [5:0] op, funct,
     input zeroM,
     output regwriteW,
@@ -13,14 +14,55 @@ module controller(
     wire memtoregD, memtoregE, memtoregM;
     wire memwriteD, memwriteE;
     wire branchD, branchE, branchM;
-    wire [2:0] alucontrolD, alucontrolE;
-    wire alusrcD, alusrcE;
-    wire regdstD, regdstE;
+    wire [2:0] alucontrolD;
+    wire alusrcD;
+    wire regdstD;
+    wire [1:0] aluop;
 
-    maindec md();
+    maindec md(.op(op),
+                .memtoreg(memtoregD), 
+                .memwrite(memwriteD),
+                .branch(branchD),
+                .alusrc(alusrcD),
+                .regdst(regdstD),
+                .regwrite(regwriteD),
+                .aluop(aluop));
 
-    aludec ad();
+    aludec ad(.funct(funct),
+                .aluop(aluop),
+                .alucontrol(alucontrolD));
 
-    
+    ID_EX_controlpipe cp1(.clk(clk),
+                .regwriteD(regwriteD),
+                .memtoregD(memtoregD), 
+                .memwriteD(memwriteD),
+                .branchD(branchD), 
+                .alusrcD(alusrcD), 
+                .regdstD(regdstD),
+                .alucontrolD(alucontrolD),
+                .regwriteE(regwriteE), 
+                .memtoregE(memtoregE), 
+                .memwriteE(memwriteE),
+                .branchE(branchE), 
+                .alusrcE(alusrcE), 
+                .regdstE(regdstE),
+                .alucontrolE(alucontrolE));
 
+    EX_MEM_controlpipe cp2(.clk(clk),
+                .regwriteE(regwriteE), 
+                .memtoregE(memtoregE), 
+                .memwriteE(memwriteE), 
+                .branchE(branchE),
+                .regwriteM(regwriteM), 
+                .memtoregM(memtoregM), 
+                .memwriteM(memwriteM),
+                .branchM(branchM));
+
+    MEM_WB_controlpipe cp3(.clk(clk),
+                .regwriteM(regwriteM), 
+                .memtoregM(memtoregM),
+                .regwriteW(regwriteW), 
+                .memtoregW(memtoregW));
+
+    assign pcsrcM = branchM && zeroM;
 endmodule
