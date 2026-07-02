f134e9e28d3a6e5ec13dcd87a4ffaf71d2aaff0b
vedsavjani <veddsavjani@gmail.com>
2026-07-02 21:07:57 +0530

    modified top module for pipelined

--- rtl/top.v
+++ rtl/top.v
@@ -1,16 +1,15 @@
 module top(
-    input clk, reset, 
-    output [31:0] writedata, dataadr,
-    output memwrite);
+    input clk, reset,
+    output [31:0] writedataM, aluoutM,
+    output memwriteM);
 
-    wire [31:0] instr, pc, readdata;
+    wire [31:0] pcF, instrF, readdataM;
 
-    mips mips_proc(.clk(clk), .reset(reset), .instr(instr),
-                   .readdata(readdata), .aluout(dataadr),
-                   .writedata(writedata), .pc(pc), .memwrite(memwrite));
+    mips m(.clk(clk), .reset(reset), .instrF(instrF), 
+            .readdataM(readdataM), .aluoutM(aluoutM), 
+            .writedataM(writedataM),
+            .pcF(pcF), .memwriteM(memwriteM));
 
-    imem instr_mem(.a(pc[7:2]), .rd(instr));
-
-    dmem data_mem(.clk(clk), .we(memwrite), .a(dataadr), .wd(writedata), .rd(readdata));
-
+    imem im(.a(pcF[7:2]), .rd(instrF));
+    dmem dm(.clk(clk), .we(memwriteM), .a(aluoutM), .wd(writedataM), .rd(readdataM));
 endmodule
