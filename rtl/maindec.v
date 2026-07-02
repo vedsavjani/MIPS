47f9a5da4a93eed37711b4a8ce1412652b0117b1
vedsavjani <veddsavjani@gmail.com>
2026-07-02 16:21:28 +0530

    removed jump from controller

--- rtl/maindec.v
+++ rtl/maindec.v
@@ -7,20 +7,20 @@ module maindec (input [5:0] op,
                 output regwrite,
                 output [1:0] aluop);
 
-    reg [8:0] controls;
+    reg [7:0] controls;
 
     always @(*) begin
         case (op)
-            6'b000000: controls = 9'b110000010; //Rtype
-            6'b100011: controls = 9'b101001000; //LW
-            6'b101011: controls = 9'b001010000; //SW
-            6'b000100: controls = 9'b000100001; //BEQ
-            6'b001000: controls = 9'b101000000; //ADDI
-            6'b000010: controls = 9'b000000100; //J
-            default:   controls = 9'bxxxxxxxxx; //??? 
+            6'b000000: controls = 8'b11000010; //Rtype
+            6'b100011: controls = 8'b10100100; //LW
+            6'b101011: controls = 8'b00101000; //SW
+            6'b000100: controls = 8'b00010001; //BEQ
+            6'b001000: controls = 8'b10100000; //ADDI
+            6'b000010: controls = 8'b00000000; //J
+            default:   controls = 8'bxxxxxxxxx; //??? 
         endcase
     end
 
-    assign {regwrite, regdst, alusrc, branch, memwrite, memtoreg, jump, aluop} = controls;
+    assign {regwrite, regdst, alusrc, branch, memwrite, memtoreg, aluop} = controls;
 
 endmodule
