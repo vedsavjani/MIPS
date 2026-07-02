47ae3c6f1778184439c5ae099bd51a8983baf4a0
vedsavjani <veddsavjani@gmail.com>
2026-07-02 19:41:43 +0530

    modified mips module

--- rtl/mips.v
+++ rtl/mips.v
@@ -11,29 +11,26 @@ module mips(
     wire zeroM;
     wire pcsrcM, memtoregW, alusrcE, regdstE, regwriteW;
 
-    controller c(
-    .clk(clk),
-    input [5:0] op, funct,
-    input zeroM,
-    output regwriteW,
-    output memtoregW,
-    output memwriteM,
-    output [2:0] alucontrolE,
-    output alusrcE,
-    output pcsrcM,
-    output regdstE);
+    controller c(.clk(clk), .reset(reset),
+                .op(instrD[31:26]), .funct(instrD[5:0]),
+                .zeroM(zeroM),
+                .regwriteW(regwriteW),
+                .memtoregW(memtoregW),
+                .memwriteM(memwriteM),
+                .alucontrolE(alucontrolE),
+                .alusrcE(alusrcE),
+                .pcsrcM(pcsrcM),
+                .regdstE(regdstE));
 
-    datapath dp(
-    input clk, reset,
-    input regwriteW, alusrcE,
-    input regdstE, memtoregW,
-    input pcsrcM,
-    input [2:0] alucontrolE,
-    input [31:0] instrF,
-    input [31:0] readdataM,
-    output zeroM,
-    output [31:0] pcF,
-    output [31:0] writedataM, aluoutM
-    output [31:0] instrD);
-
+    datapath dp(.clk(clk), .reset(reset),
+    .regwriteW(regwriteW), .alusrcE(alusrcE),
+    .regdstE(regdstE), .memtoregW(memtoregW),
+    .pcsrcM(pcsrcM),
+    .alucontrolE(alucontrolE),
+    .instrF(instrF),
+    .readdataM(readdataM),
+    .zeroM(zeroM),
+    .pcF(pcF),
+    .writedataM(writedataM), .aluoutM(aluoutM),
+    .instrD(instrD));
 endmodule
